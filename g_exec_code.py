36429264d1236e349549f1fa1cf82e337cd10308
Girijashankar Senapati <gshankar165@gmail.com>
2019-03-25 23:19:43 +0530

    Update g_exec_code.py

--- g_exec_code.py
+++ g_exec_code.py
@@ -24,10 +24,12 @@ you got rig/model updated char group from the other dep and you need to re assig
 5. just select old meshes (not group, only mesh ) which has shaders, run the script.
 
 Limitations:
-1. For Mesh Attribute copy, current I have implemented for Arnold.
+1. Both the old and new char/objects must have same mesh naming and follow proper naming convention.
+2. For Mesh Attribute copy, current I have implemented for Arnold.
  (opaque, subdivision type and iteration, UV smoothing in shape node)
-2. For other renderers support, available on request.
+3. For other renderers support, available on request.
 
+images are avilable here: https://knowledge.autodesk.com/community/article/230446
 
 ----Author-----
 Girijashankar Senapati
